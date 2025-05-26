a527cface59b3f05cef650caa04e33f3f7a721fa
NavI-Cs <rics2015ch@gmail.com>
2025-05-26 10:06:17 -0400

    login

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 Route::view('dashboard','dashboard')
-    ->middleware(['auth','verifierd'])
+    ->middleware(['auth','verified'])
     ->name('dashboard');
 
 // Ruta para gestión de usuarios

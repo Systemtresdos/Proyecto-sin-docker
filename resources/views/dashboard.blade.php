760c12fb1fe0ddf6b9595ef1022566882d399907
pablez <126174354+pablez@users.noreply.github.com>
2025-05-30 08:53:27 -0400

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,3 @@
-@extends('layouts.app')
 <x-layouts.app> 
     <h1 class="text-3xl font-bold text-gray-800">Bienvenido al Dashboard</h1>
     <p class="mt-4 text-gray-600">Desde aquí puedes navegar a las diferentes secciones.</p>
